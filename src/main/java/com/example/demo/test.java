e67d956984fcd04edb9c22f9ddcaf52c22bc5a85
TR039070 <TR039070@minhati.com.br>
2023-02-12 14:09:00 -0400

    asdasd

---
+++ src/main/java/com/example/demo/test.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/")
+    public String test(){
+        return "Funcionou";
+    }
+}
